696553a7913521ee55a3aceda0193f522ddffa96
Jake-thegangle-Snider <jakesniderlmt@gmail.com>
2019-02-25 19:50:48 -0500

    another try

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+$("#contactBtn, #closeBtn").click(function() {
+  $("#exampleModal").toggle();
+});
+
+$("#submitBtn").click(function() {
+  alert("Sorry, this feature is currently under construction. To contact Jake directly, email smokestacksandspires@gmail.com")
+});
