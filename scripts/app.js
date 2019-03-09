d96ba4ca47d2f56ff35d64214f2e561c94d1b449
Jake-thegangle-Snider <jakesniderlmt@gmail.com>
2019-03-09 11:03:51 -0500

    Image Rotation JQ

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,37 @@
 
+
 $(document).ready(function(){
     $( "#profileImage" )
   .mouseenter(function() {
-    $( this ).css({"border" : "solid teal 5px", "transform" : "rotateX(360)"});
+    $( this ).css({"border" : "solid teal 5px", "cursor" : "pointer"});
   })
   .mouseleave(function() {
     $( this ).css("border","solid white 5px");
+  });
+});
+
+//SOLUTION FOR ROTATION FOUND HERE: https://stackoverflow.com/questions/21919044/css3-transition-on-click-using-pure-css
+// SOLUTION AUTHOR: https://stackoverflow.com/users/914763/jeremyjjbrown
+
+$( ".crossRotate" ).click(function() {
+    if (  $( this ).css( "transform" ) == 'none' ){
+        $(this).css("transform","rotate(180deg)");
+    } else {
+        $(this).css("transform","" );
+    }
+});
+
+// $(document).ready(function() {
+//   $( "#profileImage" )
+//   .click(function() {
+//   rotation();
+//   window.location.href = "index.html";
+//   });
+// });
+
+$(document).ready(function() {
+  $( "#submitBtn" )
+  .click(function() {
+  alert("No server connection. To directly contact Jake Snider, Email: smokestacksandspires@gmail.com ");
   });
 });
