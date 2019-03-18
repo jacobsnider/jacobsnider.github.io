3d7c68bd004db36ab72e1637db90a3ce60368f12
Jake-thegangle-Snider <jakesniderlmt@gmail.com>
2019-03-18 19:49:16 -0400

    meta tags added

--- scripts/app.js
+++ scripts/app.js
@@ -17,7 +17,7 @@ $( ".crossRotate" ).click(function() {
     if (  $( this ).css( "transform" ) == 'none' ){
         $(this).css("transform","rotate(180deg)");
     } else {
-        $(this).css("transform", "");
+        $(this).css("transform","");
     }
 });
 
